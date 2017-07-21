2b344c789e4b954f14271f616b34bae363bd86e4
wligtenb <willem.ligtenberg@openanalytics.eu>
2017-07-21 21:52:04 +0200

    Added coin flip skill

---
+++ coin-flip-skill/__init__.py
@@ -0,0 +1,71 @@
+# Copyright 2017 Willem Ligtenberg
+#
+# Coin flip skill is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Coin flip skill is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+import random
+
+__author__ = 'Willem Ligtenberg'
+
+# Logger: used for debug lines, like "LOGGER.debug(xyz)". These
+# statements will show up in the command line when running Mycroft.
+LOGGER = getLogger(__name__)
+
+# The logic of each skill is contained within its own class, which inherits
+# base methods from the MycroftSkill class with the syntax you can see below:
+# "class ____Skill(MycroftSkill)"
+class CoinFlipSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(CoinFlipSkill, self).__init__(name="CoinFlipSkill")
+
+    # This method loads the files needed for the skill's functioning, and
+    # creates and registers each intent that the skill uses
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        coin_flip_intent = IntentBuilder("CoinFlipIntent").\
+            require("CoinFlipKeyword").build()
+        self.register_intent(coin_flip_intent, self.handle_coin_flip_intent)
+
+    # The "handle_xxxx_intent" functions define Mycroft's behavior when
+    # each of the skill's intents is triggered: in this case, he simply
+    # speaks a response. Note that the "speak_dialog" method doesn't
+    # actually speak the text it's passed--instead, that text is the filename
+    # of a file in the dialog folder, and Mycroft speaks its contents when
+    # the method is called.
+    def handle_coin_flip_intent(self, message):
+        self.speak_dialog("flip.coin")
+        if bool(random.getrandbits(1)):
+            self.speak_dialog("heads")
+        else:
+            self.speak_dialog("tails")
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, the method just contains the keyword "pass", which
+    # does nothing.
+    def stop(self):
+        pass
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return CoinFlipSkill()
