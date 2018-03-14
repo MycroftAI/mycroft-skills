c3fc49b05d2936098553e86400307e699358c532
Nhoya <nhoyadx@gmail.com>
2018-03-14 22:49:00 +0100

    Added basic example of dependencies settings depenting on distro version

--- 00__skill_template/requirements.sh
+++ 00__skill_template/requirements.sh
@@ -1,3 +1,22 @@
 # The requirements.sh is an advanced mechanism an should rarely be needed.
 # Be aware that it won't run with root permissions and 'sudo' won't work
 # in most cases.
+
+#detect distribution using lsb_release (may be replaced parsing /etc/*release)
+dist=$(lsb_release -d |awk '{print$2}')
+
+#setting dependencies and package manager in relation to the distribution
+if [ "$dist"  == "Arch"  ]; then
+    pm="pacman -S"
+    dependencies=( aaa bbb ccc )
+elif [ "$dist" ==  "Ubuntu" ] || [ "$dist" == "KDE" ] || [ "$dist" == "Debian" ]; then
+    pm="apt install"
+    dependencies=( ddd eee fff )
+fi
+
+
+# installing dependencies
+for dep in "${dependencies[@]}"
+do
+    sudo $pm $dep
+done
